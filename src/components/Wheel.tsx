4013c1a81a9296a226f820a6d74b272a0d13adca
rblondiau <ronchie.blondiau@gmail.com>
2024-08-17 18:51:53 +0200

    first commit

---
+++ src/components/Wheel.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { useState } from "react";
+import { Wheel } from "react-custom-roulette";
+import confetti from "canvas-confetti";
+
+interface PointerProps {
+  src?: string;
+  style?: React.CSSProperties; // Optional
+}
+
+const data = [
+  {
+    option: "Bitterballen",
+  },
+  {
+    option: "Frikandel",
+  },
+  {
+    option: "Kroket",
+  },
+  {
+    option: "Bicky Burger",
+  },
+  {
+    option: "Kaaskroket",
+  },
+  {
+    option: "Viandel",
+  },
+  {
+    option: "Loempia",
+  },
+  {
+    option: "Boulet",
+  },
+  {
+    option: "Satekroket",
+  },
+  {
+    option: "Vleeskroket",
+  },
+  {
+    option: "Speciaal",
+  },
+  {
+    option: "Cervela",
+  },
+  {
+    option: "Berenpoot",
+  },
+];
+
+const RouletteWheel = () => {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * data.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
+
+  return (
+    <>
+      <Wheel
+        mustStartSpinning={mustSpin}
+        prizeNumber={prizeNumber}
+        data={data}
+        backgroundColors={["#ffffff"]}
+        textColors={["#5370FE"]}
+        onStopSpinning={() => {
+          setMustSpin(false);
+          confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 },
+          });
+        }}
+        spinDuration={0.4}
+        pointerProps={{
+          src: "https://tmpfiles.nohat.cc/m2i8m2b1G6N4N4d3.png",
+          style: {
+            color: "#5370FE",
+          },
+        }}
+      />
+      <div className="pt-8">
+        <button
+          onClick={handleSpinClick}
+          className="bg-royal-blue-500 hover:bg-royal-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={mustSpin}
+        >
+          {mustSpin ? "Aant draaien ..." : "Geef er een lap op!"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default RouletteWheel;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "royal-blue": {
+          "50": "#ecf4ff",
+          "100": "#ddeaff",
+          "200": "#c2d8ff",
+          "300": "#9dbeff",
+          "400": "#7697ff",
+          "500": "#5370fe",
+          "600": "#3748f4",
+          "700": "#2b38d7",
+          "800": "#2530ae",
+          "900": "#263189",
+          "950": "#171c4f",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
